1bb00a84b026a5ccb8d69870b602e38defc4f4f3
maxlarkin <maxlarkin2007@gmail.com>
2024-02-12 16:03:08 +0400

    kamen school add bootstrap

--- src/js/Header.jsx
+++ src/js/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
 
     return (
         <div className='header'>
-            <h1>Галерея Каменской школы (v1.0.0)</h1>
+            <h1>Галерея Каменской школы (v1.2.1)</h1>
         </div>
     )
 }
